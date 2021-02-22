a52fd3732eced252d9d8dd41c289171cec7016c0
HeyyCzer <rafa.pereira.carmo@gmail.com>
2021-02-22 11:19:04 -0300

    Adicionado custo total

--- js/main.js
+++ js/main.js
@@ -29,27 +29,13 @@ $("#quantity").change(function () {
 	$("#gatilhos").val(weapons[value].gatilhos * quantity);
 
 	$("#pecasCost").val("NENHUM - FARM");
-	$("#placasCost").val("$" + formatMoney(components.placas * quantity));
-	$("#molasCost").val("$" + formatMoney(components.molas * quantity));
-	$("#gatilhosCost").val("$" + formatMoney(components.gatilhos * quantity));
+	$("#placasCost").val(formatMoney(components.placas * quantity));
+	$("#molasCost").val(formatMoney(components.molas * quantity));
+	$("#gatilhosCost").val(formatMoney(components.gatilhos * quantity));
+
+	$("#custoTotal").text(formatMoney((components.gatilhos + components.molas + components.placas) * quantity))
 });
 
-function formatMoney(number, decPlaces, decSep, thouSep) {
-	(decPlaces = isNaN((decPlaces = Math.abs(decPlaces))) ? 2 : decPlaces), (decSep = typeof decSep === "undefined" ? "." : decSep);
-	thouSep = typeof thouSep === "undefined" ? "," : thouSep;
-	var sign = number < 0 ? "-" : "";
-	var i = String(parseInt((number = Math.abs(Number(number) || 0).toFixed(decPlaces))));
-	var j = (j = i.length) > 3 ? j % 3 : 0;
-
-	return (
-		sign +
-		(j ? i.substr(0, j) + thouSep : "") +
-		i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
-		(decPlaces
-			? decSep +
-			  Math.abs(number - i)
-					.toFixed(decPlaces)
-					.slice(2)
-			: "")
-	);
+function formatMoney(number) {
+	return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(number);
 }
